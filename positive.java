b12e0f07fe477e8b450c7b8070745bec0f0a7c51
subithaMoorthy <subics96@gmail.com>
2016-09-02 15:02:02 +0530

    Create positive.java

---
+++ positive.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class positive
+{
+	public static void main (String[] args)
+	{
+		int a;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		if(a%2==0)
+		{
+			System.out.println("even number");
+		}
+		else
+		{
+			System.out.println("odd number");
+		}
+		
+		
+	}
+}
